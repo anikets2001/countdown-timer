875e24008173e756d462d0fb65e4c12457d3b097
Aniket Singh <aniketsingh90126@gmail.com>
2025-06-28 13:21:52 +0530

    feat: skeleton for countdown timer and footer -css part done

--- src/Components/CountDownTimer.jsx
+++ src/Components/CountDownTimer.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
 
 const CountDownTimer = () => {
   return (
-    <div>CountDownTimer</div>
-  )
-}
+    <div className="countdown-section">
+      <div className="countdown-wrapper">
+        <h1 className="heading">we're launching soon</h1>
 
-export default CountDownTimer
+        <div className="timer-wrapper">
+          <div className="time-box">
+            <div className="flip-card">
+              <span className="dot left-dot" />
+              <span className="dot right-dot" />
+              <div className="top" />
+              <div className="bottom" />
+              <div className="count">14</div>
+            </div>
+            <span className="time-label">Days</span>
+          </div>
+          <div className="time-box">
+            <div className="flip-card">
+              <span className="dot left-dot" />
+              <span className="dot right-dot" />
+              <div className="top" />
+              <div className="bottom" />
+              <div className="count">00</div>
+            </div>
+            <span className="time-label">Hours</span>
+          </div>
+          <div className="time-box">
+            <div className="flip-card">
+              <span className="dot left-dot" />
+              <span className="dot right-dot" />
+              <div className="top" />
+              <div className="bottom" />
+              <div className="count">00</div>
+            </div>
+            <span className="time-label">Minutes</span>
+          </div>
+          <div className="time-box">
+            <div className="flip-card">
+              <span className="dot left-dot" />
+              <span className="dot right-dot" />
+              <div className="top" />
+              <div className="bottom" />
+              <div className="count">00</div>
+            </div>
+            <span className="time-label">Seconds</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CountDownTimer;
--- src/Pages/CountDown.jsx
+++ src/Pages/CountDown.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import CountDownTimer from "../Components/CountDownTimer";
+import Footer from "../Components/Footer";
 
 const CountDown = () => {
   return (
-    <div>CountDown</div>
-  )
-}
+    <div className="countdown-page-wrapper">
+      <CountDownTimer />
+      <Footer />
+    </div>
+  );
+};
 
-export default CountDown
+export default CountDown;
