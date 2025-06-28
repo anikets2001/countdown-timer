a5b86ecde0a7ed1a2aaa38fd52c8bae9e1dc6319
Aniket Singh <aniketsingh90126@gmail.com>
2025-06-28 14:18:23 +0530

    feat: create config for timer and footer links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import Facebook from "../assets/images/icon-facebook.svg";
-import Pinterest from "../assets/images/icon-pinterest.svg";
-import Instagram from "../assets/images/icon-instagram.svg";
+import { socialLinks } from "../utils/config";
 
 const Footer = () => {
   return (
     <div className="countdown-footer">
       <div className="links-wrapper">
-        <a href="/" className="social-link">
-          <img src={Facebook} alt="Facebook" aria-label="facebook" />
-        </a>
-        <a href="/" className="social-link" aria-label="pinterest">
-          <img src={Pinterest} alt="Pinterest" />
-        </a>
-        <a href="/" className="social-link" aria-label="instagram">
-          <img src={Instagram} alt="Instagram" />
-        </a>
+        {socialLinks.map((link) => (
+          <a
+            key={link?.id}
+            href="/"
+            aria-label={link?.label}
+            className="social-link"
+            onClick={(e) => e.preventDefault()}
+          >
+            <img src={link?.icon} alt={link?.label} />
+          </a>
+        ))}
       </div>
     </div>
   );
---
+++ src/utils/config.js
@@ -0,0 +1,31 @@
+import Facebook from "../assets/images/icon-facebook.svg";
+import Pinterest from "../assets/images/icon-pinterest.svg";
+import Instagram from "../assets/images/icon-instagram.svg";
+
+export const socialLinks = [
+  {
+    id: "facebook",
+    label: "Facebook",
+    icon: Facebook,
+    href: "/",
+  },
+  {
+    id: "pinterest",
+    label: "Pinterest",
+    icon: Pinterest,
+    href: "/",
+  },
+  {
+    id: "instagram",
+    label: "Instagram",
+    icon: Instagram,
+    href: "/",
+  },
+];
+
+export const timeUnits = [
+  { id: "days", label: "Days" },
+  { id: "hours", label: "Hours" },
+  { id: "minutes", label: "Minutes" },
+  { id: "seconds", label: "Seconds" },
+];
